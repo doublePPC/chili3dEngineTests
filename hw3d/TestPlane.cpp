f192eb30732624b187f380fbd9d67dba12cc1c35
Pierre-Paul Corriveau <doubleppcorriveau@gmail.com>
2021-06-07 09:55:53 -0400

    modification de la classe TestPlane, retour à la version tuto36

--- hw3d/TestPlane.cpp
+++ hw3d/TestPlane.cpp
@@ -2,74 +2,151 @@
 #include "Plane.h"
 #include "BindableCommon.h"
 #include "imgui/imgui.h"
+#include "TransformCbufDoubleboi.h"
 
-TestPlane::TestPlane( Graphics& gfx,float size,DirectX::XMFLOAT4 color )
-	:
-	pmc( { color } )
+TestPlane::TestPlane(Graphics& gfx, float size, std::string texture)
 {
-	//using namespace Bind;
-	//namespace dx = DirectX;
+	using namespace Bind;
+	namespace dx = DirectX;
 
-	//auto model = Plane::Make();
-	//model.Transform( dx::XMMatrixScaling( size,size,1.0f ) );
-	//const auto geometryTag = "$plane." + std::to_string( size );
-	//AddBind( VertexBuffer::Resolve( gfx,geometryTag,model.vertices ) );
-	//AddBind( IndexBuffer::Resolve( gfx,geometryTag,model.indices ) );
+	auto model = Plane::Make();
+	model.Transform(dx::XMMatrixScaling(size, size, 1.0f));
+	const auto geometryTag = "$plane." + std::to_string(size);
+	AddBind(VertexBuffer::Resolve(gfx, geometryTag, model.vertices));
+	AddBind(IndexBuffer::Resolve(gfx, geometryTag, model.indices));
 
-	//auto pvs = VertexShader::Resolve( gfx,"SolidVS.cso" );
-	//auto pvsbc = pvs->GetBytecode();
-	//AddBind( std::move( pvs ) );
+	AddBind(Texture::Resolve(gfx, "Images\\brickwallTexture.jpg"));
+	AddBind(Texture::Resolve(gfx, "Images\\brickwallNormalMap.png", 2u));
 
-	//AddBind( PixelShader::Resolve( gfx,"SolidPS.cso" ) );
+	auto pvs = VertexShader::Resolve(gfx, "PhongVS.cso");
+	auto pvsbc = pvs->GetBytecode();
+	AddBind(std::move(pvs));
 
-	//AddBind( std::make_shared<PixelConstantBuffer<PSMaterialConstant>>( gfx,pmc,1u ) );
+	AddBind(PixelShader::Resolve(gfx, "PhongPSNormalMapObject.cso"));
 
-	//AddBind( InputLayout::Resolve( gfx,model.vertices.GetLayout(),pvsbc ) );
+	AddBind(PixelConstantBuffer<PSMaterialConstant>::Resolve(gfx, pmc, 1u));
 
-	//AddBind( Topology::Resolve( gfx,D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST ) );
+	AddBind(InputLayout::Resolve(gfx, model.vertices.GetLayout(), pvsbc));
 
-	//AddBind( std::make_shared<TransformCbuf>( gfx,*this,0u ) );
+	AddBind(Topology::Resolve(gfx, D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST));
 
-	//AddBind( Blender::Resolve( gfx,true,0.5f ) );
-	//
-	//AddBind( Rasterizer::Resolve( gfx,true ) );
+	AddBind(std::make_shared<TransformCbufDoubleboi>(gfx, *this, 0u, 2u));
 }
 
-void TestPlane::SetPos( DirectX::XMFLOAT3 pos ) noexcept
+TestPlane::TestPlane(Graphics& gfx, float size, std::string texture, std::string texNormalMap)
+{
+	using namespace Bind;
+	namespace dx = DirectX;
+
+	auto model = Plane::Make();
+	model.Transform(dx::XMMatrixScaling(size, size, 1.0f));
+	const auto geometryTag = "$plane." + std::to_string(size);
+	AddBind(VertexBuffer::Resolve(gfx, geometryTag, model.vertices));
+	AddBind(IndexBuffer::Resolve(gfx, geometryTag, model.indices));
+
+	AddBind(Texture::Resolve(gfx, "Images\\" + texture));
+	AddBind(Texture::Resolve(gfx, "Images\\" + texNormalMap, 2u));
+
+	auto pvs = VertexShader::Resolve(gfx, "PhongVS.cso");
+	auto pvsbc = pvs->GetBytecode();
+	AddBind(std::move(pvs));
+
+	AddBind(PixelShader::Resolve(gfx, "PhongPSNormalMapObject.cso"));
+
+	AddBind(PixelConstantBuffer<PSMaterialConstant>::Resolve(gfx, pmc, 1u));
+
+	AddBind(InputLayout::Resolve(gfx, model.vertices.GetLayout(), pvsbc));
+
+	AddBind(Topology::Resolve(gfx, D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST));
+
+	AddBind(std::make_shared<TransformCbufDoubleboi>(gfx, *this, 0u, 2u));
+}
+
+TestPlane::TestPlane(Graphics& gfx, float baseSize, float rectFactor, std::string texture, std::string texNormalMap)
+{
+	using namespace Bind;
+	namespace dx = DirectX;
+
+	auto model = Plane::Make();
+	model.Transform(dx::XMMatrixScaling(baseSize, baseSize * rectFactor, 1.0f));
+	const auto geometryTag = "$plane." + std::to_string(baseSize);
+	AddBind(VertexBuffer::Resolve(gfx, geometryTag, model.vertices));
+	AddBind(IndexBuffer::Resolve(gfx, geometryTag, model.indices));
+
+	AddBind(Texture::Resolve(gfx, "Images\\" + texture));
+	AddBind(Texture::Resolve(gfx, "Images\\" + texNormalMap, 2u));
+
+	auto pvs = VertexShader::Resolve(gfx, "PhongVS.cso");
+	auto pvsbc = pvs->GetBytecode();
+	AddBind(std::move(pvs));
+
+	AddBind(PixelShader::Resolve(gfx, "PhongPSNormalMapObject.cso"));
+
+	AddBind(PixelConstantBuffer<PSMaterialConstant>::Resolve(gfx, pmc, 1u));
+
+	AddBind(InputLayout::Resolve(gfx, model.vertices.GetLayout(), pvsbc));
+
+	AddBind(Topology::Resolve(gfx, D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST));
+
+	AddBind(std::make_shared<TransformCbufDoubleboi>(gfx, *this, 0u, 2u));
+}
+
+void TestPlane::IssueDrawCommand(DirectX::XMFLOAT3 pos, DirectX::XMFLOAT3 orientation, Graphics& gfx)
+{
+	this->SetPos(pos);
+	this->SetRotation(orientation.x, orientation.y, orientation.z);
+	this->Draw(gfx);
+	this->ResetToDefault();
+}
+
+void TestPlane::SetPos(DirectX::XMFLOAT3 pos) noexcept
 {
 	this->pos = pos;
 }
 
-void TestPlane::SetRotation( float roll,float pitch,float yaw ) noexcept
+void TestPlane::SetRotation(float roll, float pitch, float yaw) noexcept
 {
 	this->roll = roll;
 	this->pitch = pitch;
 	this->yaw = yaw;
 }
 
-DirectX::XMMATRIX TestPlane::GetTransformXM() const noexcept
+void TestPlane::ResetToDefault()
 {
-	return DirectX::XMMatrixRotationRollPitchYaw( roll,pitch,yaw ) *
-		DirectX::XMMatrixTranslation( pos.x,pos.y,pos.z );
+	this->pos = { 0.0f, 0.0f, 0.0f };
+	this->roll = 0.0f;
+	this->pitch = 0.0f;
+	this->yaw = 0.0f;
 }
 
-void TestPlane::SpawnControlWindow( Graphics& gfx,const std::string& name ) noexcept
+DirectX::XMMATRIX TestPlane::GetTransformXM() const noexcept
 {
-	//if( ImGui::Begin( name.c_str() ) )
-	//{
-	//	ImGui::Text( "Position" );
-	//	ImGui::SliderFloat( "X",&pos.x,-80.0f,80.0f,"%.1f" );
-	//	ImGui::SliderFloat( "Y",&pos.y,-80.0f,80.0f,"%.1f" );
-	//	ImGui::SliderFloat( "Z",&pos.z,-80.0f,80.0f,"%.1f" );
-	//	ImGui::Text( "Orientation" );
-	//	ImGui::SliderAngle( "Roll",&roll,-180.0f,180.0f );
-	//	ImGui::SliderAngle( "Pitch",&pitch,-180.0f,180.0f );
-	//	ImGui::SliderAngle( "Yaw",&yaw,-180.0f,180.0f );
-	//	ImGui::Text( "Shading" );
-	//	auto pBlender = QueryBindable<Bind::Blender>();
-	//	float factor = pBlender->GetFactor();
-	//	ImGui::SliderFloat( "Translucency",&factor,0.0f,1.0f );
-	//	pBlender->SetFactor( factor );
-	//}
-	//ImGui::End();
+	return DirectX::XMMatrixRotationRollPitchYaw(roll, pitch, yaw) *
+		DirectX::XMMatrixTranslation(pos.x, pos.y, pos.z);
+}
+
+void TestPlane::SpawnControlWindow(Graphics& gfx) noexcept
+{
+	if (ImGui::Begin("Plane"))
+	{
+		ImGui::Text("Position");
+		ImGui::SliderFloat("X", &pos.x, -80.0f, 80.0f, "%.1f");
+		ImGui::SliderFloat("Y", &pos.y, -80.0f, 80.0f, "%.1f");
+		ImGui::SliderFloat("Z", &pos.z, -80.0f, 80.0f, "%.1f");
+		ImGui::Text("Orientation");
+		ImGui::SliderAngle("Roll", &roll, -180.0f, 180.0f);
+		ImGui::SliderAngle("Pitch", &pitch, -180.0f, 180.0f);
+		ImGui::SliderAngle("Yaw", &yaw, -180.0f, 180.0f);
+		ImGui::Text("Shading");
+		bool changed0 = ImGui::SliderFloat("Spec. Int.", &pmc.specularIntensity, 0.0f, 1.0f);
+		bool changed1 = ImGui::SliderFloat("Spec. Power", &pmc.specularPower, 0.0f, 100.0f);
+		bool checkState = pmc.normalMappingEnabled == TRUE;
+		bool changed2 = ImGui::Checkbox("Enable Normal Map", &checkState);
+		pmc.normalMappingEnabled = checkState ? TRUE : FALSE;
+		if (changed0 || changed1 || changed2)
+		{
+			QueryBindable<Bind::PixelConstantBuffer<PSMaterialConstant>>()->Update(gfx, pmc);
+		}
+	}
+	ImGui::End();
 }
--- hw3d/TestPlane.h
+++ hw3d/TestPlane.h
@@ -1,21 +1,31 @@
 #pragma once
 #include "Drawable.h"
+#include <string>
 
 class TestPlane : public Drawable
 {
+private:
+	void SetPos(DirectX::XMFLOAT3 pos) noexcept;
+	void SetRotation(float roll, float pitch, float yaw) noexcept;
+	void ResetToDefault();
 public:
-	TestPlane( Graphics& gfx,float size,DirectX::XMFLOAT4 color = { 1.0f,1.0f,1.0f,0.0f } );
-	void SetPos( DirectX::XMFLOAT3 pos ) noexcept;
-	void SetRotation( float roll,float pitch,float yaw ) noexcept;
+	TestPlane(Graphics& gfx, float size, std::string texture);
+	TestPlane(Graphics& gfx, float size, std::string texture, std::string texNormal);
+	TestPlane(Graphics& gfx, float baseSize, float rectFactor, std::string texture, std::string texNormal);
+	void IssueDrawCommand(DirectX::XMFLOAT3 pos, DirectX::XMFLOAT3 orientation, Graphics& gfx);
 	DirectX::XMMATRIX GetTransformXM() const noexcept override;
-	void SpawnControlWindow( Graphics& gfx,const std::string& name ) noexcept;
+	void SpawnControlWindow(Graphics& gfx) noexcept;
 private:
 	struct PSMaterialConstant
 	{
-		DirectX::XMFLOAT4 color;
+		float specularIntensity = 0.18f;
+		float specularPower = 18.0f;
+		BOOL normalMappingEnabled = TRUE;
+		float padding[1];
 	} pmc;
 	DirectX::XMFLOAT3 pos = { 0.0f,0.0f,0.0f };
 	float roll = 0.0f;
 	float pitch = 0.0f;
 	float yaw = 0.0f;
+	std::string  texDirectory;
 };
